60f4dd7fd229e735d9a74e08b0f9b0c7b6c91be4
nirnai <rnirnai@gmail.com>
2020-02-17 22:53:59 +0100

    removed comments

--- src/euclideanCluster/KDTree.hpp
+++ src/euclideanCluster/KDTree.hpp
@@ -56,7 +56,6 @@ struct KdTree
 
 	}
 
-	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(const PointT& target, float distanceTol)
 	{
 		std::vector<int> indexes;
@@ -88,25 +87,6 @@ struct KdTree
 				search(p, node->left, depth+1, distanceTol, indexes);
 			if(p.data[dim] + distanceTol > node->point.data[dim])
 				search(p, node->right, depth+1, distanceTol, indexes);
-			// Eigen::Vector3f p_curr  = node->point.getVector3fMap(); 
-			// Eigen::Vector3f p       = target.getVector3fMap();
-			// Eigen::Array3f minPoint = node->point.getArray3fMap() - distanceTol;
-			// Eigen::Array3f maxPoint = node->point.getArray3fMap() + distanceTol;
-
-			// bool inCube = (minPoint < p.array()).all() && ( p.array() < maxPoint).all();
-            // if ( inCube )
-			// {
-			// 	float distance = 1;//(p - p_curr).norm();
-			// 	if(distance < distanceTol)
-			// 		indexes.push_back(node->index);
-			// }
-
-
-			// uint dim = depth % 3;
-			// if(p[dim] - distanceTol < p_curr[dim])
-			// 	search(p, node->left, depth+1, distanceTol, indexes);
-			// if(p[dim] + distanceTol > p_curr[dim])
-			// 	search(p, node->right, depth+1, distanceTol, indexes);
 		}
 	}
 	
